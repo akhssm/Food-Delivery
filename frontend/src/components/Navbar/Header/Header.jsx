f4fb7af4e43a917a03a58fa201941120c38841a1
Akshay <mandaakshay20@gmail.com>
2024-09-18 11:43:51 +0530

    created header folder and added image.

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse featuring a delectable array of dishes crafted with the finest ingredients and culinary expertised, satify your carvings and elevate your dining experience, one delicious meal at a time.</p>
+        <button>ViewMenu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
