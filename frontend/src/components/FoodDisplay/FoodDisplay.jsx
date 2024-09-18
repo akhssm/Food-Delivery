bae9983847b8dc435bb52fd116f9b15056d87f66
Akshay <mandaakshay20@gmail.com>
2024-09-18 16:40:19 +0530

    added food name, mage, price, description.

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { storeContext } from '../../context/storeContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
 
@@ -11,7 +12,7 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
             })}
         </div>
     </div>
