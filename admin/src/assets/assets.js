f3ba0dfd727aec85cfa4a5081ca6ed131be12225
Akshay <mandaakshay20@gmail.com>
2026-07-24 16:53:43 +0530

    Updated assests.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-bvnh.onrender.com'
+export const url = 'https://food-delivery-backend-7ntt.onrender.com'
